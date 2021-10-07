1890218089f8690b6f0c2477aa4e217041ab9798
INSORTIAL <renwell2099@gmail.com>
2021-10-06 23:33:52 -0700

    Added new functions to pass in

--- employeeTwo.c
+++ employeeTwo.c
@@ -21,6 +21,17 @@ static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableVa
     return strcmp((char *) targetPtr, tableValuePtr->name);
 }
 
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return * (double *) targetPtr != tableValuePtr->salary;
+}
+
+static int compareEmployeePhoneNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return strcmp((char *) targetPtr, tableValuePtr->phone);
+}
+
+
 PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number)
 {
     return searchEmployeeTable(ptr, size, &number, compareEmployeeNumber);
@@ -29,4 +40,14 @@ PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long numb
 PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char* name)
 {
     return searchEmployeeTable(ptr, size, name, compareEmployeeName);
+}
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary)
+{
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeNumber);
+}
+
+PtrToEmployee searchEmployeeByPhoneNumber(PtrToConstEmployee ptr, int size, char* phoneNumber)
+{
+    return searchEmployeeTable(ptr, size, phoneNumber, compareEmployeeName);
 }
